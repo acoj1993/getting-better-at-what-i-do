1c7422f9e96805c33e8a2b6432929895339af5c3
Aleksandar Jankovic <a.jankovic1993@gmail.com>
2022-03-27 18:23:40 +0200

    Initial Commit

---
+++ src/main/java/com/acoj1993/playground/PlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.acoj1993.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundApplication.class, args);
+	}
+
+}
